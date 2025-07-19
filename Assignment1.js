9872b4a9dff92c817f44aff7fe44a9b2bdfa7000
pavankumar <pavankumar@onoark.com>
2025-07-19 12:28:54 +0800

    testing

---
+++ Assignment1.js
@@ -0,0 +1,32 @@
+
+//Task1 
+var x=10;
+var y="Hello";
+var result= x+y;
+console.log("result:"+result);
+
+//Task2
+
+function checkoddoreven(input1)
+{
+
+var num1=input1;
+
+if(num1%2==0)
+{
+console.log("Even");
+}
+else
+{
+console.log("Odd");
+}
+
+}
+
+//Task3
+function greetUser(name)
+{
+console.log("Hello, "+name+"!");
+} 
+
+//Task4
